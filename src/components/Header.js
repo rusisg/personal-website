6bc5348be54f7d674ba99f03d4b2681a078fbd12
Rusisg <rasul.nulrybekov@gmail.com>
2023-03-04 15:48:33 +0500

    template

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className={"header_container"}>
+            Hello world!
+        </div>
+    );
+};
+
+export default Header;
